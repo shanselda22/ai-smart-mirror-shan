e125877fb581a49acaaed891a203785b9eb1e694
shanselda22 <shanselda@gmail.com>
2017-03-26 13:39:49 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 pip install virtualenv
 virtualenv hhsmartmirror
-source ./hhsmartmirror/bin/activate
+source ./shanselda22/bin/activate
 pip install -r requirements.txt
 pip install --global-option='build_ext' --global-option='-I/usr/local/include' --global-option='-L/usr/local/lib' pyaudio
